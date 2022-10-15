19e14d8fd463d005456220744184391245c0d048
digvijaysingh21 <67684163+digvijaysingh21@users.noreply.github.com>
2022-10-15 16:04:14 +0530

    Added solution - LeetHub

--- Add two numbers represented by linked lists - GFG/add-two-numbers-represented-by-linked-lists.cpp
+++ Add two numbers represented by linked lists - GFG/add-two-numbers-represented-by-linked-lists.cpp
@@ -92,42 +92,28 @@ class Solution
         int carry = 0;
         Node* ansHead = NULL;
         Node* ansTail = NULL;
-        while(first != NULL && second != NULL){
-            int sum = carry + first-> data + second-> data;
+        
+        while(first != NULL || second != NULL || carry != 0){
+            
+            int val1=0;
+            if(first !=NULL)
+            val1 = first->data;
+            
+            int val2 =0;
+            if(second != NULL)
+            val2= second-> data;
+            int sum = carry + val1 + val2;
             int digit = sum%10;
             
             //create node and add in answer linked list
             insertAtTail(ansHead, ansTail, digit);
             carry = sum/10;
-            first = first-> next;
-            second= second -> next;
             
-        }
-        
-        while(first != NULL){
-        int sum = carry + first-> data;
-        int digit = sum%10;
-        //create node and add in answer
-        insertAtTail(ansHead, ansTail, digit);
-        carry= sum/10;
-        first= first-> next;
-        }
-        
-        while(second != NULL){
-        int sum = carry + second-> data;
-        int digit = sum%10;
-        //create node and add in answer
-        insertAtTail(ansHead, ansTail, digit);
-        carry= sum/10;
-        second= second-> next;
-        }
-    
-    while(carry != 0){
-        int sum = carry ;
-        int digit = sum%10;
-        //create node and add in answer
-        insertAtTail(ansHead, ansTail, digit);
-        carry= sum/10;
+            if(first != NULL)
+            first = first-> next;
+            
+            if(second != NULL)
+            second= second -> next;
         }
         return ansHead;
     }
