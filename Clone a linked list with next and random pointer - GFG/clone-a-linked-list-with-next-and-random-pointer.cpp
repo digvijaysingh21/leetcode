6f4747d558ad31a63464e71da735a7f12e22c4f2
digvijaysingh21 <67684163+digvijaysingh21@users.noreply.github.com>
2022-10-16 05:18:20 +0530

    Added solution - LeetHub

--- Clone a linked list with next and random pointer - GFG/clone-a-linked-list-with-next-and-random-pointer.cpp
+++ Clone a linked list with next and random pointer - GFG/clone-a-linked-list-with-next-and-random-pointer.cpp
@@ -20,58 +20,93 @@ struct Node {
 // } Driver Code Ends
 class Solution
 {
- private:
- void insertAtTail(Node* &head, Node* &tail, int d){
-     Node* newNode = new Node(d);
-     if(head == NULL){
-         head = newNode;
-         tail = newNode;
-         return;
-     }
-     else{
-         tail-> next = newNode;
-         tail = newNode;
-     }
- }
- 
+    
+    private:
+    void insertAtTail(Node* &head, Node* &tail, int d){
+        
+        Node* newNode = new Node(d);
+        
+        if(head == NULL){
+           head = newNode;
+           tail = newNode;
+        }
+        else{
+            tail-> next = newNode;
+            tail = newNode;
+        }
+}
     public:
     Node *copyList(Node *head)
     {
-        //creating a clone list
+        //Write your code here
+        
+        //step1 create a clone list
+        
         Node* cloneHead = NULL;
         Node* cloneTail = NULL;
         
-        Node * temp = head;
-        
+        Node* temp = head;
         while(temp != NULL){
-            insertAtTail(cloneHead, cloneTail, temp->data);
-            temp = temp-> next;
+            insertAtTail(cloneHead, cloneTail, temp-> data);
+            temp = temp->next;
         }
         
-        //creating a map
-        unordered_map<Node*,Node*> oldToNewNode;
         
         
-       Node* originalNode = head;
+        //step 2 cloneNode add in between original list
         
+        Node* originalNode = head;
         Node* cloneNode = cloneHead;
         
         while(originalNode != NULL && cloneNode != NULL){
-            oldToNewNode[originalNode] = cloneNode;
-            originalNode = originalNode-> next;
-            cloneNode = cloneNode-> next;
+            Node* next = originalNode -> next;
+            originalNode-> next = cloneNode;
+            originalNode = next;
             
+            next = cloneNode-> next;
+            cloneNode-> next = originalNode;
+            cloneNode = next;
         }
         
-        originalNode = head;
-        cloneNode = cloneHead;
         
-        while( originalNode != NULL){
+        //step3 random pointer copy
+         temp = head;
+        
+        while(temp != NULL){
             
-            cloneNode -> arb = oldToNewNode[originalNode-> arb];
-            originalNode = originalNode -> next;
+            if(temp-> next != NULL){
+                
+                temp-> next -> arb = temp-> arb ? temp -> arb-> next : temp-> arb;
+            }
+            
+            /*
+            if(temp-> next != NULL){
+                if(temp-> random != NULL){
+                   temp-> next -> random = temp-> random-> next; 
+                }
+                else{
+                    temp-> next = temp-> random
+                }
+                
+            }
+            */
+        temp = temp-> next -> next;
+    }
+        
+         // step4 revert changes done in step2
+         originalNode = head;
+         cloneNode = cloneHead;
+           while(originalNode != NULL && cloneNode != NULL){
+            originalNode-> next = cloneNode-> next;
+            originalNode = originalNode-> next;
+           
+           if(originalNode != NULL){ 
+            cloneNode-> next = originalNode -> next;
+           }
             cloneNode = cloneNode -> next;
         }
+        
+        //step5 retur ans;
         return cloneHead;
     }
 
