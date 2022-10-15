34c79ab14b6ab741e29cfcca39f8ae77b847eb5a
digvijaysingh21 <67684163+digvijaysingh21@users.noreply.github.com>
2022-10-15 10:27:44 +0530

    Added solution - LeetHub

--- Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
+++ Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
@@ -28,34 +28,66 @@ struct Node {
   }
 };
 */
-#include<vector>
+
 class Solution{
-    private:
-    bool checkPalindrome(vector<int> arr){
-        int n = arr.size();
-        int s= 0;
-        int e = n-1;
-        while(s<= e){
-            if(arr[s] != arr[e]){
-                return 0;
-            }
-            s++;
-            e--;
-        }
-        return 1;
-    }
   public:
-    //Function to check whether the list is palindrome.
+  Node* getMid(Node* head){
+      
+      Node* slow = head;
+      Node* fast = head-> next;
+      while(fast != NULL && fast-> next != NULL){
+          fast = fast-> next-> next;
+          slow= slow-> next;
+      }
+      return slow;
+  }
+  
+  Node* reverse(Node* head){ 
+      
+    Node* curr = head;
+    Node* next = NULL;
+    Node* prev = NULL;
+    
+    while(curr != NULL){
+        next = curr-> next;
+        curr-> next = prev;
+        prev = curr;
+        curr= next;
+    }
+    return prev;
+}
+
+//Function to check whether the list is palindrome.
     bool isPalindrome(Node *head)
     {
         //Your code here
-        vector<int> arr;
-        Node* temp = head;
-        while(temp != NULL){
-            arr.push_back(temp-> data);
-            temp = temp-> next;
+        
+        if(head -> next == NULL){
+            return true;
         }
-        return checkPalindrome(arr);
+        //find middle
+        Node* middle = getMid(head);
+        
+        //reverse after middle
+        Node* temp = middle-> next;
+        middle-> next = reverse(temp);
+        
+        //compare both halves
+        Node* head1= head;
+        Node* head2 = middle-> next;
+        while(head2 != NULL){
+            if(head1->data != head2-> data){
+            return false;
+        }
+        head1 = head1->next;
+        head2 = head2-> next;
+        }
+        
+        //repeat step2
+        
+        temp = middle-> next;
+        middle-> next = reverse(temp);
+        return true;
     }
 };
 
