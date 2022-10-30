d541cc5bb6cf9407f28b7f44f8e455910f8974ac
digvijaysingh21 <67684163+digvijaysingh21@users.noreply.github.com>
2022-10-30 21:31:16 +0530

    Added solution - LeetHub

---
+++ Construct Tree from Inorder & Preorder - GFG/construct-tree-from-inorder-preorder.cpp
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+//
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+void printPostOrder(Node *root)
+{
+	if(root==NULL)
+		return;
+	printPostOrder(root->left);
+	printPostOrder(root->right);
+	cout<<root->data<<" ";
+}
+
+// } Driver Code Ends
+
+
+/*Complete the code here.
+Node is as follows:
+struct Node
+{
+  int data;
+  Node* left;
+  Node* right;
+};
+*/
+class Solution{
+    public:
+    
+    int findPosition(int in[], int element, int n){
+        for(int i=0; i< n; i++){
+            if(in[i] == element)
+            return i;
+        }
+        return -1;
+    }
+    Node* solve(int in[], int pre[], int &index, int inorderStart, int inoredrEnd, int n){
+        //bas case
+        if(index >= n || inorderStart > inoredrEnd){
+            return NULL;
+        }
+        
+        int element = pre[index++];
+        Node* root = new Node(element);
+        int position = findPosition(in, element, n);
+        
+        //recursive call
+        root-> left = solve(in, pre, index, inorderStart, position - 1, n);
+        root-> right = solve(in, pre, index, position+1, inoredrEnd, n);
+        return root;
+    }
+    
+    Node* buildTree(int in[],int pre[], int n)
+    {
+        int preOrderIndex = 0;
+        Node* ans = solve(in, pre, preOrderIndex, 0, n-1, n);
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		
+		int inorder[n], preorder[n];
+		for(int i=0; i<n; i++)
+			cin>> inorder[i];
+		for(int i=0; i<n; i++)
+			cin>> preorder[i];
+		Solution obj;
+		Node *root = obj.buildTree(inorder, preorder, n);
+		printPostOrder(root);
+		cout<< endl;
+	}
+}
+
+// } Driver Code Ends
