c1cb4738af75079a1394c1f67eb4c3a1dfd43187
digvijaysingh21 <67684163+digvijaysingh21@users.noreply.github.com>
2022-11-10 10:50:29 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -10,28 +10,36 @@ using namespace std;
 
 class Solution{
     private:
+    //in safe functions we are checking three conditions
+    //1.  position should be inside the matrix
+    //2.  array value should be open that is 1 for travel
+    //3.  visited should be marked 0
     bool isSafe(int x, int y, int n, vector<vector<int > > visited, 
                 vector<vector<int>> &m){
-        if( (x >= 0 && x<n) && (y >= 0 && y<n) && visited[x][y] ==0 && m[x][y]== 1){
+        if( (x >= 0 && x<n) && (y >= 0 && y<n) && visited[x][y] == 0 && m[x][y]== 1){
         return true;
         }
         else{
             return false;
         }
     }
+    //solve function
     void solve(vector<vector<int>> &m, int n, vector<string>& ans, int x, int y, 
                vector<vector<int > > visited, string path){
         
+        //we have reached x, y here
         
         //base case
         if(x == n-1 && y == n-1){
             ans.push_back(path);
             return;
         }
+        
+        //mark visited =  1
         visited[x][y] = 1;
         
-        //4 choice s down , up , left , right
         
+        //4 choices of movement down, up, left, right and here x and y value changes
         
         //down
          int newx = x+1;
@@ -69,29 +77,42 @@ class Solution{
             solve(m, n, ans, newx, newy, visited, path);
             path.pop_back();
          }
-        
+         
+        // when back track mark visited 0 to get other paths possible
       visited[x][y] = 0;
     }
     public:
     vector<string> findPath(vector<vector<int>> &m, int n) {
         // Your code goes here
         
+        //to store ans
         vector<string> ans;
+        
+        //srtarting position is 0 then
         if(m[0][0] == 0){
             return ans;
         }
+        
+        //source at start is (0,0)
         int srcx = 0;
         int srcy = 0;
        
+       //visited array (2d array) to keep track of path visited
         vector<vector<int > > visited = m;
-        //initialise with zero
+        //initialise with 0
         for(int i =0; i<n; i++){
             for(int j = 0; j<n; j++){
                 visited[i][j] = 0;
             }
         }
+        
+        //path which is a empty string
         string path = "";
+        
+        //recursive functional call
         solve(m, n, ans, srcx, srcy, visited, path);
+       
+        //answer in sorted order
         sort(ans.begin(), ans.end());
         return ans;
     }
